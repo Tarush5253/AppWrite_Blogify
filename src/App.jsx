634fbc35a8f670fbdb5608338c092d27cfb08fe6
tarush <tarushruhela1234@gmail.com>
2024-07-25 12:36:21 +0530

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {login , logout } from './store/authSlice';
 import authService from "./appwrite/auth";
 import {Header , Footer} from "./components"
 import {Outlet} from "react-router-dom";
+import "./App.css"
 
 function App() {
   const [loading , setLoading] = useState(true);
@@ -36,7 +37,7 @@ function App() {
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400 overflow-y-scroll no-scrollbar'>
       <div className='w-full min-h-screen flex flex-col'>
         <Header />
-        <main className="min-h-screen py-4 px-2 sm:p-4 lg:p-4 bg-[url('https://media.istockphoto.com/id/1422421435/photo/man-reading-business-email-newsletter-on-smartphone-man-looking-mailbox-customer-contract.jpg?s=1024x1024&w=is&k=20&c=utVHzN5ZElD-wJ-Y0kgBdR4Ows9dabHtfQUDNLib0uQ=')] bg-cover bg-center">
+        <main className="main-height py-4 px-2 sm:p-4 lg:p-4 bg-[url('https://media.istockphoto.com/id/1422421435/photo/man-reading-business-email-newsletter-on-smartphone-man-looking-mailbox-customer-contract.jpg?s=1024x1024&w=is&k=20&c=utVHzN5ZElD-wJ-Y0kgBdR4Ows9dabHtfQUDNLib0uQ=')] bg-cover bg-center">
         <Outlet />
         </main>
         <Footer />
